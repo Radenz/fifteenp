77cd618d8a9a5054bf2b92f4f4c53b9da336bfe6
Radenz <radenrifqirahman@gmail.com>
2022-03-31 21:02:07 +0700

    refactor: Rename Application class name

--- src/main/java/id/ac/itb/stei/informatika/fifteenp/Application.java
+++ src/main/java/id/ac/itb/stei/informatika/fifteenp/Application.java
@@ -1,16 +1,15 @@
 package id.ac.itb.stei.informatika.fifteenp;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 720, 540);
         stage.setTitle("Hello!");
         stage.setScene(scene);
