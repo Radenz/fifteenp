7b20b4e801dcb9268beeb92a0a7103d2485593c2
Radenz <radenrifqirahman@gmail.com>
2022-04-01 23:54:22 +0700

    docs: Document FifteenMatrixBuilder and Direction class

--- src/main/java/id/ac/itb/stei/informatika/fifteenp/util/Direction.java
+++ src/main/java/id/ac/itb/stei/informatika/fifteenp/util/Direction.java
@@ -1,11 +1,30 @@
 package id.ac.itb.stei.informatika.fifteenp.util;
 
+/**
+ * {@code Direction} enumerates 4 equidistant discrete
+ * directions.
+ */
 public enum Direction {
+    /**
+     * Upwards direction.
+     */
     UP,
+    /**
+     * Downwards direction.
+     */
     DOWN,
+    /**
+     * Rightwards direction.
+     */
     RIGHT,
+    /**
+     * Leftwards direction.
+     */
     LEFT;
 
+    /**
+     * All available directions.
+     */
     public static final Direction[] DIRECTIONS = {
             Direction.DOWN,
             Direction.RIGHT,
@@ -13,6 +32,12 @@ public enum Direction {
             Direction.UP,
     };
 
+    /**
+     * Flips a {@code Direction} into its opposite
+     * direction, identical to rotating the direction
+     * 180 degrees.
+     * @return the opposite direction of this {@code Direction}
+     */
     public Direction flip() {
         return switch (this) {
             case UP -> Direction.DOWN;
--- src/main/java/id/ac/itb/stei/informatika/fifteenp/util/FifteenMatrixBuilder.java
+++ src/main/java/id/ac/itb/stei/informatika/fifteenp/util/FifteenMatrixBuilder.java
@@ -2,10 +2,22 @@ package id.ac.itb.stei.informatika.fifteenp.util;
 
 import java.util.ArrayList;
 
+/**
+ * {@code FifteenMatrixBuilder} is a builder class to
+ * build a {@code FifteenMatrix} by enumerating its
+ * elements sequentially. It will throw various exceptions
+ * on any errors to prevent creating an invalid
+ * {@code FifteenMatrix} object.
+ * @see FifteenMatrix
+ */
 public class FifteenMatrixBuilder {
     private Integer[] values;
     private int cursor;
 
+    /**
+     * Creates a new {@code FifteenMatrixBuilder} object
+     * and initialize its elements sequence to empty.
+     */
     public FifteenMatrixBuilder() {
         this.values = new Integer[16];
         for (int i = 0; i < 16; i++) {
@@ -14,7 +26,21 @@ public class FifteenMatrixBuilder {
         this.cursor = 0;
     }
 
-    public FifteenMatrixBuilder append(Integer value) {
+    /**
+     * Appends an element into the elements sequence
+     * of the {@code FifteenMatrixBuilder}. The appending
+     * is done in-place and the modified
+     * {@code FifteenMatrixBuilder} is also returned
+     * to enable method chaining.
+     * @param value the element to append
+     * @return the appended {@code FifteenMatrixBuilder}
+     * @throws IllegalArgumentException if the element to
+     *         append is not a valid element
+     * @throws IndexOutOfBoundsException if the builder
+     *         has 16 elements sequence already
+     */
+    public FifteenMatrixBuilder append(Integer value)
+        throws IllegalArgumentException, IndexOutOfBoundsException {
         if (value != null && (value < 0 || value > 15)) {
             throw new IllegalArgumentException();
         }
@@ -29,7 +55,16 @@ public class FifteenMatrixBuilder {
         return this;
     }
 
-    public FifteenMatrix build() {
+    /**
+     * Builds a {@code FifteenMatrix} object based on
+     * the elements sequence appended to this
+     * {@code FifteenMatrixBuilder}.
+     * @return a new {@code FifteenMatrix} object
+     * @throws IllegalArgumentException if there are
+     *         any duplicate value in the elements
+     *         sequence
+     */
+    public FifteenMatrix build() throws IllegalArgumentException {
         FifteenMatrix puzzle = new FifteenMatrix();
         ArrayList<Boolean> flags = new ArrayList<>();
 
