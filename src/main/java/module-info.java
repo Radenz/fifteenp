a47940db7959dbce182f50dc32c75f66a9a87f25
Radenz <radenrifqirahman@gmail.com>
2022-04-03 14:49:37 +0700

    chore: Remove unused dependencies and specify different jar output directory

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,6 @@ module id.ac.itb.stei.informatika.fifteenp {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-    requires org.kordamp.bootstrapfx.core;
-
     opens id.ac.itb.stei.informatika.fifteenp to javafx.fxml;
     exports id.ac.itb.stei.informatika.fifteenp;
 }
